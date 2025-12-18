737ba7f5b90e7f2050b9134015b92ab86c07e48b
LeandroBarreiro <leandrobarreiro15@gmail.com>
2025-12-18 17:18:35 -0400

    modified:   src/controllers/products-controller.ts 	new file:   src/controllers/tables-controller.ts 	new file:   src/controllers/tables-sessions-controller.ts 	modified:   src/database/database.db 	new file:   src/database/migrations/20251218173130_create-tables.ts 	new file:   src/database/migrations/20251218180653_create-tables-sessions.ts 	new file:   src/database/migrations/20251218203730_create-orders.ts 	new file:   src/database/seeds/insert-products.ts 	new file:   src/database/seeds/insert-table.ts 	new file:   src/database/types/table-repository.d.ts 	new file:   src/database/types/tables-sessions-repository.d.ts 	modified:   src/routes/index.ts 	modified:   src/routes/products-routes.ts 	new file:   src/routes/tables-routes.ts 	new file:   src/routes/tables-sessions-routes.ts

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,11 +1,17 @@
 import { NextFunction, Request, Response } from "express"
+import { AppError } from "../utils/AppError"
 import { knex } from "../database/knex"
 import { z } from "zod"
 
 class ProductController {
     async index(request: Request, response: Response, next: NextFunction) {
         try {
-            const products = await knex<ProductRepository>("products").select().whereLike("name",).orderBy("name")
+
+            const { name } = request.query
+            const products = await knex<ProductRepository>("products")
+            .select()
+            .whereLike("name", `%${name ?? ""}%`)
+            .orderBy("name")
 
 
             return response.json(products)
@@ -30,6 +36,71 @@ class ProductController {
             next(error)
         }
     }
+
+
+    async update(request: Request, response: Response, next: NextFunction){
+        try {
+
+            const id = z.string()
+            .transform((value) => Number(value))
+            .refine((value) => !isNaN(value), { message: "id must be a number" })
+            .parse(request.params.id)
+
+            const bodySchema = z.object({
+                name: z.string({ required_error: "Name is required!"}).trim().min(6),
+                price: z.number().gt(0, { message: "value must be greater than 0" })
+            })
+
+            const { name, price } = bodySchema.parse(request.body)
+
+            const product = await knex<ProductRepository>("products")
+            .select()
+            .where({ id })
+            .first()
+
+            if(!product) {
+                throw new AppError("product not found")
+            }
+            
+
+            await knex<ProductRepository>("products")
+            .update({ name, price, updated_at: knex.fn.now() })
+            .where({ id })
+
+            return response.status(200).json({ message: "Product updated successfully"})
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async remove(request: Request, response: Response, next: NextFunction){
+        try {
+             const id = z.string()
+            .transform((value) => Number(value))
+            .refine((value) => !isNaN(value), { message: "id must be a number" })
+            .parse(request.params.id)
+
+            const product = await knex<ProductRepository>("products")
+            .select()
+            .where({ id })
+            .first()
+            
+            console.log(product);
+            
+
+            if(!product) {
+                throw new AppError("product not found")
+            }
+
+            await knex<ProductRepository>("products").delete().where({ id })
+
+            
+            return response.status(200).json()
+        } catch (error) {
+            next(error)
+        }
+    }
+
 }
 
 
---
+++ src/controllers/tables-sessions-controller.ts
@@ -0,0 +1,88 @@
+import { Request, Response, NextFunction } from "express";
+import { knex } from "../database/knex";
+import { z } from "zod"
+import { AppError } from "../utils/AppError";
+
+class TablesSessionsController {
+    async create(request: Request, response: Response, next: NextFunction){
+        try {
+
+            const bodySchema = z.object({
+                table_id: z.number(),
+            })
+
+            const { table_id } = bodySchema.parse(request.body)
+
+            const session = await knex<tablesSessionsRepository>("tables_sessions")
+            .select()
+            .where({table_id})
+            .orderBy("opened_at", "desc")
+            .first()
+
+            if(session && !session.closed_at) {
+                throw new AppError("this table is already open")
+            }
+
+            await knex<tablesSessionsRepository>("tables_sessions").insert({
+                table_id,
+                opened_at: knex.fn.now(),
+
+
+            })
+
+            return response.status(201).json()
+        } catch (error) {
+            next(error)
+        }
+    }
+
+
+    async index(request: Request, response: Response, next: NextFunction){
+        try {
+            const sessions = await knex<tablesSessionsRepository>("tables_sessions")
+            .select()
+            .orderBy("closed_at")
+
+            return response.json(sessions)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async update(request: Request, response: Response, next: NextFunction){
+        try {
+            const id = z
+                .string()
+                .transform((value) => Number(value))
+                .refine((value) => !isNaN(value), { message: "id must be a number"})
+                .parse(request.params.id)
+
+            const session = await knex<tablesSessionsRepository>("tables_sessions")
+            .select()
+            .where({ id })
+            .first()
+
+            if(!session) {
+                throw new AppError("session table not found")
+            }
+
+            if(session.closed_at) {
+                throw new AppError("this session tables is already closed!")
+            }
+
+            await knex<tablesSessionsRepository>("tables_sessions")
+            .update({
+                closed_at: knex.fn.now()
+            })
+            .where({ id })
+
+            return response.json()
+        } catch (error) {
+            next(error)
+        }
+    }
+
+}
+
+
+export { TablesSessionsController }
